7c9cc45579288650568c45a60be359daf6fd93bb
Alex Balleste <alex.balleste@gmail.com>
2015-07-03 11:47:44 +0200

    Demo vista agenda

--- www/modules/agendaEvents/js/agenda_events_ctrl.js
+++ www/modules/agendaEvents/js/agenda_events_ctrl.js
@@ -1,28 +1,64 @@
-angular.module('starter.agendaevents')
-
-.controller('AgendaEventsCtrl',['$scope','$ionicModal','$timeout','AgendaEventsService','$stateParams', function($scope, $ionicModal, $timeout,AgendaEventsService,$stateParams) {
+angular.module('starter.agendaevents')  
+    .controller('AgendaEventsCtrl',['$scope','$ionicModal','$interval','AgendaEventsService','$location','$stateParams', function($scope, $ionicModal, $interval,AgendaEventsService,$location,$stateParams) {
     
-    $scope.title = "Agenda Events"; 
+    $scope.title = "Activitats"; 
+    $scope.fullEvents = false;
+    $scope.firstIndex = -1;
+    $scope.secIndex = 0;
+    $scope.events  = [];
+    $scope.eventsList = [];
+    $scope.agendaList = [];
+    var changeEvents = {};
+    
+    $scope.$on('$ionicView.enter', function() {
+        //Load the new messages
+        if ($location.$$url === '/app/agendaevents'){
+            var expandButton = { 'text' : 'Totes', 'onclick' : function () {
+                    if ($scope.fullEvents){
+                        this.text = 'Totes';
+                    }else{
+                        this.text = 'Recull';
+                    }
+
+                    $scope.fullEvents= !$scope.fullEvents;    
+
+                }
+            };
+                                
+            $scope.$parent.extraButtons = [expandButton];
+        }
+    });
+    
+    $scope.$on('$stateChangeStart', function() {
+        $scope.$parent.extraButtons = null;
+    })
     
     $scope.initList = function (){
-         $scope.agendaEventsList  = AgendaEventsService.getAgendaEvents();
+        $scope.eventsList  = AgendaEventsService.getEvents();
+        $scope.agendaList = AgendaEventsService.getAgendaItems();
+        
+        if ($scope.events.length > 1){
+            $scope.secIndex = 1;
+        }
+        
     };
     
     $scope.initEvent = function (){
         
         //In the case we access directly from event 
-        if ($scope.agendaEventsList===undefined || $scope.agendaEventsList.lenght <= 0){
+        if ($scope.agendaList===undefined || $scope.agendaList.length <= 0){
             $scope.initList();
         }
         
         //Look for the agenda Event
-        for (var i = 0; i < $scope.agendaEventsList.length; i++){
-            if ($scope.agendaEventsList[i].id ===  $stateParams.agendaEventId){
-                $scope.currentEvent = $scope.agendaEventsList [i];
+        for (var i = 0; i < $scope.agendaList.length; i++){
+            if ($scope.agendaList[i].id ===  $stateParams.agendaEventId){
+                $scope.currentEvent = $scope.agendaList [i];
                 break;
             }
         }
     };
-    
+
 }]);
+
 
--- www/modules/agendaEvents/js/agenda_events_service.js
+++ www/modules/agendaEvents/js/agenda_events_service.js
@@ -1,7 +1,7 @@
 angular.module('starter.agendaevents', [])
 .service('AgendaEventsService',[function (){
  
-    this.getAgendaEvents = function (){
+    this.getEvents = function (){
         var genericDescription = 'Lorem Ipsum és un text de farciment usat per la indústria de la tipografia i la impremta. Lorem Ipsum ha estat el text estàndard de la indústria des de l\'any 1500, quan un impressor desconegut va fer servir una galerada de text i la va mesclar per crear un llibre de mostres tipogràfiques. No només ha sobreviscut cinc segles, sinó que ha fet el salt cap a la creació de tipus de lletra electrònics, romanent essencialment sense canvis. Es va popularitzar l\'any 1960 amb el llançament de fulls Letraset que contenien passatges de Lorem Ipsum, i més recentment amb programari d\'autoedició com Aldus Pagemaker que inclou versions de Lorem Ipsum.';
         
         
@@ -13,4 +13,22 @@ angular.module('starter.agendaevents', [])
                 {imgSrc: 'modules/customBuildService/agendaMockup/img/event6.jpg', title: 'La Udl millora les collites amb noves tècniques', id: '6', description: genericDescription , date: '3 Nov 2014'  }];
         return items;
     };
+    
+    this.getAgendaItems = function (){
+        
+        var genericDescription = 'Lorem Ipsum és un text de farciment usat per la indústria de la tipografia i la impremta. Lorem Ipsum ha estat el text estàndard de la indústria des de l\'any 1500, quan un impressor desconegut va fer servir una galerada de text i la va mesclar per crear un llibre de mostres tipogràfiques. No només ha sobreviscut cinc segles, sinó que ha fet el salt cap a la creació de tipus de lletra electrònics, romanent essencialment sense canvis. Es va popularitzar l\'any 1960 amb el llançament de fulls Letraset que contenien passatges de Lorem Ipsum, i més recentment amb programari d\'autoedició com Aldus Pagemaker que inclou versions de Lorem Ipsum.';
+
+        var nowdate = moment();
+        var yesterday = moment().subtract(1,'days');
+        var lastweek = moment().subtract(1,'weeks');
+        
+        
+        var agendaItems = [{title: 'Congrès de veterinaria', id: '1', description: genericDescription, dayOfWeek: nowdate.format ('dddd'), dayMonth: nowdate.format('D MMM'), hour: nowdate.format('HH:mm'), location: 'Seu vella'},
+                           {title: 'Congrès de dddd', id: '2', description: genericDescription, dayOfWeek: yesterday.format ('dddd'), dayMonth: yesterday.format('D MMM'), hour: yesterday.format('HH:mm'), location: 'Sala de juntes'},
+                           {title: 'Premis de dddd',  id: '3', description: genericDescription, dayOfWeek: lastweek.format ('dddd'), dayMonth: lastweek.format('D MMM'), hour: lastweek.format ('HH:mm'),location: 'Rectorat - Sala d\'actes'},
+                           {title: 'Acampada a la UdL', id: '4', description: genericDescription, dayOfWeek: nowdate.format ('dddd'), dayMonth: nowdate.format('D MMM'), hour: '12:00', location: 'Edifici emblemàtic - 3.10'}
+                          ];
+        return agendaItems;
+    };
+    
 }]);
