c7aab3e94e5cb1bcd3c6f5a1c3389998191c8a25
Alex Balleste <alex.balleste@gmail.com>
2015-12-03 10:16:08 +0100

    AMU-94 Dropping table and create with new mechanishm to see if has to be recreated

--- www/modules/activities/js/activity_service.js
+++ www/modules/activities/js/activity_service.js
@@ -21,14 +21,17 @@ angular.module('starter.activities',[])
     };
 
     var queries = {
-        'CREATE_TABLE' : 'CREATE TABLE IF NOT EXISTS activity_items (id text primary key, title text, content text, location text,period text, startdate date,duedate date,published boolean,state text,image mediumtext,eventurl text)',
+        'DROP_TABLE': 'DROP TABLE IF EXISTS activity_items',
+        'CREATE_TABLE' : 'CREATE TABLE IF NOT EXISTS activity_items (id text primary key, title text, content text, location text,period text, duedate date, published boolean,state text,image mediumtext, eventurl text)',
         'SELECT_ITEMS' : 'SELECT * FROM activity_items ORDER BY date(duedate)',
-        'INSERT_ITEM' : 'INSERT INTO activity_items (title, content, location, period, startdate, duedate, published, image, eventurl,state, id) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
-        'UPDATE_ITEM' : 'UPDATE activity_items SET title=?, content=?, location=?, period=?, startdate=?, duedate=?, published=?,image=?,eventurl=?,state=? WHERE id=?',
+        'INSERT_ITEM' : 'INSERT INTO activity_items (title, content, location, period, duedate, published, image, eventurl, state, id) VALUES (?,?,?,?,?,?,?,?,?,?)',
+        'UPDATE_ITEM' : 'UPDATE activity_items SET title=?, content=?, location=?, period=?, duedate=?, published=?,image=?,eventurl=?,state=? WHERE id=?',
         'PURGE_ITEMS' :'DELETE FROM activity_items WHERE state=? or  published = 0',
         'DELETE_ALL_ITEMS' :'DELETE FROM activity_items'
     };
 
+    var activityTableSchemaVersion = '1.1';
+    
     var actService = this;
     actService.ready = $q.defer();
     actService.retrieving = false;
@@ -47,7 +50,6 @@ angular.module('starter.activities',[])
 
                         for (var i=0;i < res.rows.length; i++){
                             var row = res.rows.item(i);
-                            var itemDate = moment(row.startDate);
 
                             var item = {
                                 _id: row.id,
@@ -57,15 +59,9 @@ angular.module('starter.activities',[])
                                 period: row.period,
                                 state: row.state,
                                 published: row.published,
-                                startDate: row.startDate,
                                 dueDate: row.duedate,
                                 image: row.image,
                                 eventURL: row.eventurl,
-                                hour: itemDate.format('HH:mm'),
-                                eventDay: itemDate.startOf('day'),
-                                month: itemDate.month(),
-                                dayOfMonth: itemDate.format('D'),
-                                dayOfWeek: itemDate.isoWeekday()
                             };
 
                             activityItems.unshift (item);
@@ -92,7 +88,7 @@ angular.module('starter.activities',[])
 
         DBService.getDb().then (function (db){
             db.transaction(function(tx) {
-                tx.executeSql(create ? queries.INSERT_ITEM : queries.UPDATE_ITEM , [item.title, item.content, item.location,item.period, item.startDate,item.dueDate,item.published,item.image,item.eventURL, item.state, item._id ], 
+                tx.executeSql(create ? queries.INSERT_ITEM : queries.UPDATE_ITEM , [item.title, item.content, item.location,item.period,item.dueDate,item.published,item.image,item.eventURL, item.state, item._id ], 
                               function (tx,res) {
                     defered.resolve (item);
                 },
@@ -129,7 +125,6 @@ angular.module('starter.activities',[])
                     //afegim totes les propietats per defecte per tenir un objecte sencer...
                     activityItem.created = Date.now;
                     activityItem.lastUpdate = Date.now;
-                    activityItem.startDate = Date.now;
                     activityItem.dueDate = {};
                     activityItem.title = "";
                     activityItem.period = "";
@@ -145,15 +140,6 @@ angular.module('starter.activities',[])
                 if(foundItem!==undefined || (foundItem===undefined && !discarded)){                    
                     //Add the item into the array and save it to the db
                     actService.saveItem (activityItem, (foundItem===undefined)).then (function (item){
-                        var itemDate = moment(item.eventDate);
-
-                        item.dayMonth =  itemDate.format('D');
-                        item.month = itemDate.month();
-                        item.dayOfWeek = itemDate.isoWeekday();
-
-                        item.hour = itemDate.format('HH:mm');
-                        item.eventDay =  itemDate.startOf('day');
-
                         if (foundItem!==undefined){
                             angular.extend (foundItem,item);
                         }else if (!discarded) {
@@ -186,19 +172,49 @@ angular.module('starter.activities',[])
             //Create activity Table
             DBService.getDb().then (function (db){
                 db.transaction(function(tx) {
+                    
+                    var createTable = function (){
+                        tx.executeSql(queries.CREATE_TABLE, [], 
+                            function (tx,res) {
+                                    console.log (activityDef.name + ' initialized');
+                                    localStorage.activityTableSchemaVersion = activityTableSchemaVersion;
+                                    
+                                    //Delete the date to force all items reload
+                                    if (localStorage.lastActivityDate){
+                                        delete localStorage.lastActivityDate;
+                                    }
+                                    actService.ready.resolve();
+                                    
+                            },
+                            function (tx,err){
+                                    console.log ('Error creating the table' + err.message);
+                                    actService.ready.reject (errorCodes.ERROR_CREATING_TABLE);
+                        });
+                    };
+                    
+                    var dropAndCreateTable = function (){
+                        tx.executeSql(queries.DROP_TABLE, [], 
+                        
+                        function (tx,res) {
+                            console.log (activityDef.name + ' table dropped');
+                            //We trigger the table creation
+                            createTable();
+                        },
+                        function (tx,err){
+                            console.log ('Error dropping the table' + err.message);
+                            actService.ready.reject (errorCodes.ERROR_CREATING_TABLE);
+                        });
+                    };
+                    
                     //create a table it doesn't exist
-                    tx.executeSql(queries.CREATE_TABLE, [], 
-                                  function (tx,res) {
-                        console.log (activityDef.name + ' initialized');
-                        actService.ready.resolve();
-                    },
-                                  function (tx,err){
-                        console.log ('Error creating the table' + err.message);
-                        actService.ready.reject (errorCodes.ERROR_CREATING_TABLE);
+                    if (localStorage.activityTableSchemaVersion === undefined || localStorage.activityTableSchemaVersion !== activityTableSchemaVersion){
+                        dropAndCreateTable();
+                    } else {
+                        //Try to create for the case for any reason it's not already created and 
+                        createTable();
                     }
-                                 );
                 },
-                               function (txerror){
+                     function (txerror){
                     console.log ('Error getting the transaction for creting table' + txerror.message);    
                     actService.ready.reject (errorCodes.ERROR_CREATING_TABLE);
                 });
